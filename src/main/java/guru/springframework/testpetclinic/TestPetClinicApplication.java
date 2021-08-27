b9df65a0d878873934199f48adb22d77239e4afb
ItsMaker <25mugl@gmail.com>
2021-08-28 01:09:26 +0300

    Initial commit

---
+++ src/main/java/guru/springframework/testpetclinic/TestPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.testpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/testpetclinic/TestPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.testpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
