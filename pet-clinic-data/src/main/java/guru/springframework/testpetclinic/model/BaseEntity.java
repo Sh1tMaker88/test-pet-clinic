7c2229881b56bf39b3b929662feda6895c8d5e8e
ItsMaker <25mugl@gmail.com>
2021-08-29 22:58:59 +0300

    Create base entity. Close #10

--- pet-clinic-data/src/main/java/guru/springframework/testpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/testpetclinic/model/BaseEntity.java
@@ -3,11 +3,12 @@ package guru.springframework.testpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class Person extends BaseEntity {
+public class BaseEntity implements Serializable {
 
-    private String firstName;
+    private Long id;
 
-    private String lastName;
 }
