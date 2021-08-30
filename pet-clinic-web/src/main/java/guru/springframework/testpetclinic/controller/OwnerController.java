66f7befadfd77e85cfb117be04a8dd5d6a05e244
ItsMaker <25mugl@gmail.com>
2021-08-30 23:15:30 +0300

    Add view for owners. Close #16

--- pet-clinic-web/src/main/java/guru/springframework/testpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/testpetclinic/controller/OwnerController.java
@@ -1,13 +1,22 @@
 package guru.springframework.testpetclinic.controller;
 
+import guru.springframework.testpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping("/owners")
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
