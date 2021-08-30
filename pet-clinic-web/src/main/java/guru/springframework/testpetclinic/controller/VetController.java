1c83e9cc6a25fd595443ba7c507fedfe5a7eec8c
ItsMaker <25mugl@gmail.com>
2021-08-30 23:26:26 +0300

    Add view for vet. Close #15

--- pet-clinic-web/src/main/java/guru/springframework/testpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/testpetclinic/controller/VetController.java
@@ -1,14 +1,22 @@
 package guru.springframework.testpetclinic.controller;
 
+import guru.springframework.testpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
-    @GetMapping("/vets")
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping("/vets")
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
