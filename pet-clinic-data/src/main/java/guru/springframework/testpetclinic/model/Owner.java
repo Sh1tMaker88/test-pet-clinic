16a969e35a799b5f7aeace47fb8237f1c7593720
ItsMaker <25mugl@gmail.com>
2021-09-05 00:37:51 +0300

    Add contact info for owner entity. Close #28

--- pet-clinic-data/src/main/java/guru/springframework/testpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/testpetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package guru.springframework.testpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+@Getter
+@Setter
 public class Owner extends Person {
+
+    private String address;
+
+    private String city;
+
+    private String telephone;
 
     private Set<Pet> pets;
 }
