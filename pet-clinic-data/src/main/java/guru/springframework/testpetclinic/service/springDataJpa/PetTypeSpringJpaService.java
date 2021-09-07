ebe148c75792d4e8f58dfdb2fd47304bb21a85f3
ItsMaker <25mugl@gmail.com>
2021-09-07 22:32:44 +0300

    Create petType spring data service. Close #42

---
+++ pet-clinic-data/src/main/java/guru/springframework/testpetclinic/service/springDataJpa/PetTypeSpringJpaService.java
@@ -0,0 +1,48 @@
+package guru.springframework.testpetclinic.service.springDataJpa;
+
+import guru.springframework.testpetclinic.model.PetType;
+import guru.springframework.testpetclinic.repository.PetTypeRepository;
+import guru.springframework.testpetclinic.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class PetTypeSpringJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSpringJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElseThrow(() -> new RuntimeException("No such element with ID:" + id));
+    }
+
+    @Override
+    public PetType save(PetType entity) {
+        return petTypeRepository.save(entity);
+    }
+
+    @Override
+    public void delete(PetType entity) {
+        petTypeRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
