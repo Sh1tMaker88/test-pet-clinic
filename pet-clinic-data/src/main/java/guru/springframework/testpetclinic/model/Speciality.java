b4e19ff1901921056b894c223c6885ce4829e2d3
ItsMaker <25mugl@gmail.com>
2021-09-07 19:12:45 +0300

    Update vet and Speciality entity with JPA annotations. Close #37

--- pet-clinic-data/src/main/java/guru/springframework/testpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/testpetclinic/model/Speciality.java
@@ -5,11 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "desscription")
     private String description;
 }
