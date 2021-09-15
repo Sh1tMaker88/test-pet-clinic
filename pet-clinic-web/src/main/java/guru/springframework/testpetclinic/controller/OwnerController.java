3ea03229d4c382a57fa581bbced3966ef8d10773
ItsMaker <25mugl@gmail.com>
2021-09-15 19:19:27 +0300

    Add way to display owner. Close #53

--- pet-clinic-web/src/main/java/guru/springframework/testpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/testpetclinic/controller/OwnerController.java
@@ -4,7 +4,9 @@ import guru.springframework.testpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -26,5 +28,13 @@ public class OwnerController {
     public String findOwner() {
 
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+
+        return mav;
     }
 }
