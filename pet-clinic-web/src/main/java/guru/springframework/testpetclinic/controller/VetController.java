02a9765cbeb8680a10e1dc15dbdea777e378044d
ItsMaker <25mugl@gmail.com>
2021-09-18 23:32:41 +0300

    Add JSON endpoint for vets. Closes #66

--- pet-clinic-web/src/main/java/guru/springframework/testpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/testpetclinic/controller/VetController.java
@@ -1,9 +1,13 @@
 package guru.springframework.testpetclinic.controller;
 
+import guru.springframework.testpetclinic.model.Vet;
 import guru.springframework.testpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +24,8 @@ public class VetController {
         return "vets/index";
     }
 
-
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
 }
